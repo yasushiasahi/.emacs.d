6c226fd29b7ace005bf276239cfa19e0205d9a78
Yasushi Asahi <asahi1600@gmail.com>
2019-04-20 01:30:18 +0900

    line hilight

--- init.el
+++ init.el
@@ -121,10 +121,13 @@
 (column-number-mode t) ; カラム番号を表示
 (size-indication-mode t) ; ファイスサイズを表示
 
+:: 現在行のハイライト
+(global-hl-line-mode t)
+
 ;; 対応する括弧の強調表示
 (setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
 (show-paren-mode t) ; 有効化
-(setq show-paren-style 'parenthesis) ; カッコのみをハイライト
+(setq show-paren-style 'mixed) ; カッコのみをハイライト
 
 ;; カーソルの色
 (set-cursor-color "#ff00ff")
